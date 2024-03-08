7f8d57f537874881adc3fe635260f203995243e9
Piyush2k23 <piyush23pr2000@gmail.com>
2024-03-08 20:08:01 +0530

    Initial commit for setup project

---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    createdAt: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
